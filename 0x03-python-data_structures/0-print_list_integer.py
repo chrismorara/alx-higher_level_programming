8004bacca302cfdd3d8f30af3e4f9ea863246430
chrismorara <chrismorara@gmail.com>
2022-06-07 14:44:09 +0000

    List of integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
         for eles in my_list:
-                    print("{:d}".format(eles))ii
+                    print("{:d}".format(eles))
