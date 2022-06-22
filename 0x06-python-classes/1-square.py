427d3f57155d88cd0d9d0ff5a580104d15aafbbb
chrismorara <chrismorara@gmail.com>
2022-06-22 21:46:32 +0000

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Defining class Square"""
+
+
+class Square:
+    """Adding a class method"""
+
+    def __init__(self, size):
+        """Run when new instance of class defined
+        Args:
+            size (int): dimensions of new instance
+        """
+        self.__size = size
